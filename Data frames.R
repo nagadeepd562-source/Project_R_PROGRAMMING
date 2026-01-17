98a3057f3ffc37d147d632f392c23f21e80d44b0
nagadeepd562-source <nagadeepd562@gmail.com>
2026-01-16 23:08:55 -0600

    Data frames using adam datasets

---
+++ Data frames.R
@@ -0,0 +1,28 @@
+#install the package admiral and this is used to create adam dataset.
+install.packages("admiral")
+library(admiral) #loading the admiral package
+library(admiraldev) #loading the admiraldev package
+adlb_dataset<-admiral::admiral_adlb # we are loading the datasets 
+View(adlb_dataset)
+class(adlb_dataset)
+dim(adlb_dataset)
+names(adlb_dataset)
+#read the csv into environment
+csv_file<-read.csv("input_dataset.csv") #reading the input_dataset.csv file from local(working directory)
+copyofdatase<-read.csv("First_Application/CopyOfinput_dataset.csv") #reading the CopyOfinput_dataset.csv file from local (from different working directory)
+copy_adlbdatase<- haven::read_sas("/Users/gouthamigorantla/Desktop/Data Analyst Marketing/CopyOfadlb_dataset.sas7bdat")
+View(copy_adlbdatase)
+dim(csv_file)
+getwd()
+#install the package haven
+install.packages("haven")
+library(haven)
+haven::write_sas(adlb_dataset, "adlb_dataset.sas7bdat")
+result<-haven::read_sas("adlb_dataset.sas7bdat")
+result
+class(result)
+dim(adlb_dataset)
+factor(adlb_dataset$LBTESTCD)
+names(adlb_dataset)
+factor(adlb_dataset$DOMAIN)
+adsl_dataset<-admiral::admiral_adsl #loading adsl dataset from admiral package
