9bda5db7a3d14266ce29bf4512848244cda9fced
nagadeepd562-source <nagadeepd562@gmail.com>
2026-01-19 22:53:35 -0600

    Data_frames updated concepts

--- Data frames.R
+++ Data frames.R
@@ -2,14 +2,14 @@
 install.packages("admiral")
 library(admiral) #loading the admiral package
 library(admiraldev) #loading the admiraldev package
-adlb_dataset<-admiral::admiral_adlb # we are loading the datasets 
+adlb_dataset<-admiral::admiral_adlb # we are loading the datasets in to environment 
 View(adlb_dataset)
 class(adlb_dataset)
 dim(adlb_dataset)
 names(adlb_dataset)
 #read the csv into environment
 csv_file<-read.csv("input_dataset.csv") #reading the input_dataset.csv file from local(working directory)
-copyofdatase<-read.csv("First_Application/CopyOfinput_dataset.csv") #reading the CopyOfinput_dataset.csv file from local (from different working directory)
+copyofdatase<-read.csv("First_Application/input_dataset.csv") #reading the input_dataset.csv file from local (from different working directory)
 copy_adlbdatase<- haven::read_sas("/Users/gouthamigorantla/Desktop/Data Analyst Marketing/CopyOfadlb_dataset.sas7bdat")
 View(copy_adlbdatase)
 dim(csv_file)
@@ -21,7 +21,7 @@ haven::write_sas(adlb_dataset, "adlb_dataset.sas7bdat")
 result<-haven::read_sas("adlb_dataset.sas7bdat")
 result
 class(result)
-dim(adlb_dataset)
+Sdim(adlb_dataset)
 factor(adlb_dataset$LBTESTCD)
 names(adlb_dataset)
 factor(adlb_dataset$DOMAIN)
